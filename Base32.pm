b47e284e2fd50e1f1cac127aa38289ef4568ead6
Daniel Peder <Daniel.Peder@INFOSET.cz>
2003-12-11 16:39:25 +0000

    Import of DANPEDER/CGI-Session-ID-Base32-1.01 from CPAN.
    gitpan-cpan-distribution: CGI-Session-ID-Base32
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         DANPEDER/CGI-Session-ID-Base32-1.01.tar.gz
    gitpan-cpan-author:       DANPEDER
    gitpan-cpan-maturity:     released

--- Base32.pm
+++ Base32.pm
@@ -1,13 +1,13 @@
 package CGI::Session::ID::Base32;
 
-# $Id: Base32.pm_rev 1.4 2003/12/11 13:40:10 root Exp root $
+# $Id: Base32.pm_rev 1.5 2003/12/11 16:31:09 root Exp root $
 
 use strict;
 use MIME::Base32 qw( 09AV );
 
 use vars qw( $VERSION );
 
-	$VERSION = '1.00';
+	$VERSION = '1.01';
 
 sub generate_id {
     my $self = shift;
